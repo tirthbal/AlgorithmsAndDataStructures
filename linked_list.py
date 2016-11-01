6827e7b38bf8662fa5a933b6018b6bff1ba2a2b2
sherlcode13 <tirthprakashbal@gmail.com>
2016-11-02 01:07:17 +0530

    Add insertion and deletion by key in linked list

---
+++ linked_list.py
@@ -0,0 +1,94 @@
+# Node class
+class Node:
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data  # Assign data
+        self.next = None  # Initialize next as null
+
+
+# Linked list class
+class LinkedList:
+    # Constructor to initialize the linked list object
+    def __init__(self):
+        self.head = None
+
+    # Traverse a singly linked list
+    def display(self):
+        node = self.head
+        while node is not None:
+            print node.data,
+            node = node.next
+            if node is not None:
+                print '->',
+            else:
+                print
+
+    # Inserting element in front
+    def push(self, data):
+        newnode = Node(data)
+        newnode.next = self.head
+        self.head = newnode
+
+    # Inserting element after given node
+    def insert_after(self, prev_node, data):
+        newnode = Node(data)
+        newnode.next = prev_node.next
+        prev_node.next = newnode
+
+    # Append node in the end
+    def append(self, data):
+        newnode = Node(data)
+        if self.head is None:
+            self.head = newnode
+            return
+        last = self.head
+        while last.next is not None:
+            last = last.next
+        last.next = newnode
+
+    # Delete a node given key
+    def deleteNode(self, key):
+        temp = self.head
+        if temp is not None:
+            if temp.data == key:
+                self.head = temp.next
+                temp = None
+                return
+        while temp is not None:
+            if temp.data == key:
+                break
+            prev = temp
+            temp = temp.next
+        if temp is None:
+            return
+        prev.next = temp.next
+        temp = None
+        return
+
+# Code execution starts here
+if __name__ == '__main__':
+    # Start with empty list
+    llist = LinkedList()
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+    # Three nodes have been created
+    llist.head.next = second
+    second.next = third
+    # Printing the linked list
+    llist.display()
+    # Inseting two nodes in front
+    llist.push(0)
+    llist.push(-1)
+    # Printing updated list
+    llist.display()
+    # Appending new node
+    llist.append(4)
+    llist.append(5)
+    # Printing updated list
+    llist.display()
+    # Delete -1, 3
+    llist.deleteNode(-1)
+    llist.deleteNode(3)
+    # Printing updated list
+    llist.display()
