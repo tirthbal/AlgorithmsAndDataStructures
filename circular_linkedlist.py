63a7197874d1fd193a9a4c9d01322981822d93fa
sherlcode13 <tirthprakashbal@gmail.com>
2017-01-12 21:16:31 +0530

    add class for circular linked list and insert at begining in cll

--- circular_linkedlist.py
+++ circular_linkedlist.py
@@ -1,11 +1,49 @@
+# Node class
+class Node:
+    # Constructor to initialize the node object
+    def __init__(self, data=None):
+        self.data = data  # Assign data
+        self.next = None  # Initialize next as null
 
+# Circular Linked List
+class CircularLinkedList:
 
-def printList(head):
-	ptr = head
-	if ptr != None:
-		print ptr.data
-		while True:
-			ptr = ptr.next
-			if ptr == head:
-				break
+	# Constructor for circular linked list
+	def __init__(self):
+		self.head = None
+
+	# Insert node in the begining of circular linked list
+	def push(self, data):
+		ptr = Node(data)
+		temp = self.head
+
+		ptr.next = self.head
+
+		if self.head is not None:
+			while (temp.next != self.head):
+				temp = temp.next
+			temp.next = ptr
+		else:
+			ptr.next = ptr
+
+		self.head = ptr
+
+	# Traversal in the circular linked list
+	def printList(self):
+		ptr = self.head
+
+		if ptr != None:
 			print ptr.data
+			while True:
+				ptr = ptr.next
+				if ptr == self.head:
+					break
+				print ptr.data
+
+
+# Code execution starts here
+if __name__ == '__main__':
+	cll = CircularLinkedList()
+	for i in [4, 3 , 2, 1]:
+		cll.push(i)
+	cll.printList()
