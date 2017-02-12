8ad8e383f6e8d266c17db707e6a809d26eecb6d7
sherlcode13 <tirthprakashbal@gmail.com>
2017-02-12 18:47:44 +0530

    fix a bug in reverse a stack using recursion

--- reversing_a_stack.py
+++ reversing_a_stack.py
@@ -1,4 +1,4 @@
-from stack import Stack
+from stack_ll import Stack_ll
 
 
 ''' 
@@ -7,18 +7,19 @@ Input
 Output
 2 1 3 4 5
 '''
-
+rev_stack = Stack_ll()
 def recur(st):
 	if not st.get_top():
 		return
 	ele = st.pop()
+	rev_stack.push(ele)
 	recur(st)
-	st.push(ele)
 
-st = Stack()
+st = Stack_ll()
 for i in [5, 4, 3, 1, 2]:
 	st.push(i) 
 
 recur(st)
-while not st.get_top():
-	print st.pop()
+
+while rev_stack.get_top():
+	print rev_stack.pop()
