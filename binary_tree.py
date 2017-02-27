10c55745dfb343ddbbe8b841a5b9f1df4b440ec5
sherlcode13 <tirthprakashbal@gmail.com>
2017-02-28 00:49:31 +0530

    add inorder, preorder, postorder tree traversals in the binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -9,3 +9,24 @@ class Tree:
 
 	def __init__(self, data):
 		self.root = Node(data)
+
+	def inorder(self, curnode):
+		if not curnode:
+			return
+		self.inorder(curnode.left)
+		print curnode.data
+		self.inorder(curnode.right)
+
+	def preorder(self, curnode):
+		if not curnode:
+			return
+		print curnode.data
+		self.preorder(curnode.left)
+		self.preorder(curnode.right)
+
+	def postorder(self, curnode):
+		if not curnode:
+			return
+		self.postorder(curnode.left)
+		self.postorder(curnode.right)
+		print curnode.data
