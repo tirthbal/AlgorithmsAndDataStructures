b02828c4ebd5e3e9d0b5f19bc6971d20d326d176
sherlcode13 <tirthprakashbal@gmail.com>
2017-03-17 23:34:20 +0530

    add method to connect to connect all the node at same level

--- binary_special_tree.py
+++ binary_special_tree.py
@@ -13,3 +13,22 @@ class BinarySpecialTree:
 
 	def __init__(self):
 		self.root = Node()
+
+	def connect_right_node(self):
+		if not self.root:
+			return
+
+		qu = Queue()
+		qu.enqueue({node: self.root, level: 1})
+		prev_front = {node: None, level: 0}
+		while qu.get_front():
+			front = qu.get_front()
+			qu.enqueue()
+			if front.left:
+				qu.enqueue({node: front.left, level: front.level + 1})
+			if front.right:
+				qu.enqueue({node: front.right, level: front.level + 1})
+			if not prev_front.node || prev_front.level != front.level:
+				prev_front = front
+				continue
+			prev_front.node.next_right = front
