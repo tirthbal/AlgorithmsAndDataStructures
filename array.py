77e6d99383274df774e481b5b3545039d8b5f990
sherlcode10 <tirthprakashbal@gmail.com>
2017-04-05 20:13:26 +0530

    add method to insert in a sorted array

--- array.py
+++ array.py
@@ -16,16 +16,40 @@ class Array:
 
 	def binary_search(self, key):
 
+		# To use binary search array must be sorted
+
 		beg,end = 0, len(self.array)
 
 		while beg <= end:
 			mid = (beg + end)/2
 			if self.array[mid] == key:
 				print 'Key found'
+				return mid
 				break
 			elif self.array[mid] < key:
 				beg = mid + 1
 			else:
 				end = mid - 1
 
-		print 'Key not found' 
+		print 'Key not found'
+		return beg
+
+	def insert_in_sorted(self, key):
+
+		# find the upper bound of the key
+		pos = self.binary_search(key)
+
+		# Store the data after pos to end in
+		# a temporary buffer
+		tmp = []
+		for i range(pos, len(self.array)):
+			tmp.append(self.array[i])
+
+		# Store the data from start to pos - 1 in the new array
+		new_array = self.array[:pos]
+		new_array.append(key)
+		new_array += tmp
+		del tmp
+		self.array = new_array
+
+
